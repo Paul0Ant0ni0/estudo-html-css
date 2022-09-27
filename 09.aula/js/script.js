7a117c8043c85d86d2604274317a65770ce44dad
Paulo Antonio <dev.pauloantonio785@gmail.com>
2022-09-27 12:10:34 -0300

    Menu navbar e responsividade

---
+++ 09.aula/js/script.js
@@ -0,0 +1,8 @@
+// Js vai varrer o documento html
+// em busca do elemento com class .navbar-menu
+// toggle = coloca se não tiver, remove se já tiver
+
+function ativaNavbar(){
+   document.querySelector(".navbar-menu").classList.toggle("ativa")
+   
+}
